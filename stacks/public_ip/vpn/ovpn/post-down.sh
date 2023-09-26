3f0c8e481422e8b2ca1247151ca5d533721dcd2b
Sam McKelvie <dev@mckelvie.org>
2023-09-26 16:58:36 -0700

    remove dead stacks

--- stacks/public_ip/vpn/ovpn/post-down.sh
+++
@@ -1,9 +0,0 @@
-sudo ip -4 rule delete table 51820
-sudo ip -4 rule delete table main suppress_prefixlength 0
-sudo ip link delete dev ovpn-dmz
-cat <<EOF | sudo nft -f -
-#!/usr/sbin/nft -f
-
-table ip wg-quick-ovpn-dmz
-delete table ip wg-quick-ovpn-dmz
-EOF
